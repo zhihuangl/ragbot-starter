53951fdfb8a76e8efa84080606c58980382906a2
Brian <brianshao20@gmail.com>
2024-08-15 00:03:23 -0400

    Reverted to old set up while commenting out all new additions.

--- app/api/setup/route.ts
+++ app/api/setup/route.ts
@@ -1,38 +1,38 @@
-import { NextResponse } from 'next/server'
-import { Pinecone } from '@pinecone-database/pinecone'
-import { TextLoader } from 'langchain/document_loaders/fs/text'
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
-import { DirectoryLoader } from 'langchain/document_loaders/fs/directory'
-import {
-    createPineconeIndex,
-    updatePinecone
-} from '../../../util'
-import { indexName } from '../../../config'
+// import { NextResponse } from 'next/server'
+// import { Pinecone } from '@pinecone-database/pinecone'
+// import { TextLoader } from 'langchain/document_loaders/fs/text'
+// import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+// import { DirectoryLoader } from 'langchain/document_loaders/fs/directory'
+// import {
+//     createPineconeIndex,
+//     updatePinecone
+// } from '../../../util'
+// import { indexName } from '../../../config'
 
-//Used to interact with pinecone functions
-export async function POST() {
-    //getting all documents that we're storing
-    const loader = new DirectoryLoader('./documents', {
-        ".txt": (path) => new TextLoader(path),
-        ".md": (path) => new TextLoader(path),
-        ".pdf": (path) => new PDFLoader(path),
-    })
+// //Used to interact with pinecone functions
+// export async function POST() {
+//     //getting all documents that we're storing
+//     const loader = new DirectoryLoader('./documents', {
+//         ".txt": (path) => new TextLoader(path),
+//         ".md": (path) => new TextLoader(path),
+//         ".pdf": (path) => new PDFLoader(path),
+//     })
 
-    const docs = await loader.load()
-    const vectorDimensions = 1536
+//     const docs = await loader.load()
+//     const vectorDimensions = 1536
 
-    const client = new Pinecone({
-      apiKey: process.env.PINECONE_API_KEY,
-    });
+//     const client = new Pinecone({
+//       apiKey: process.env.PINECONE_API_KEY,
+//     });
 
-    try {
-        await createPineconeIndex(client, indexName, vectorDimensions)
-        await updatePinecone(client, indexName, docs)
-    } catch (err) {
-        console.log("ERROR: " , err)
-    }
+//     try {
+//         await createPineconeIndex(client, indexName, vectorDimensions)
+//         await updatePinecone(client, indexName, docs)
+//     } catch (err) {
+//         console.log("ERROR: " , err)
+//     }
 
-    return NextResponse.json({
-        data: "Successfully created index and loaded data into pinecone"
-    })
-}
+//     return NextResponse.json({
+//         data: "Successfully created index and loaded data into pinecone"
+//     })
+// }
--- app/page.tsx
+++ app/page.tsx
@@ -7,124 +7,66 @@ import PromptSuggestionRow from '../components/PromptSuggestions/PromptSuggestio
 import ThemeButton from '../components/ThemeButton';
 import useConfiguration from './hooks/useConfiguration';
 
-
-// export default function Home() {
-//   const { append, messages, input, handleInputChange, handleSubmit } = useChat();
-//   const { useRag, llm, similarityMetric, setConfiguration } = useConfiguration();
-//   const messagesEndRef = useRef(null);
-
-//   const scrollToBottom = () => {
-//     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-//   };
-
-//   useEffect(() => {
-//     scrollToBottom();
-//   }, [messages]);
-
-//   const handleSend = (e) => {
-//     handleSubmit(e, { options: { body: { useRag, llm, similarityMetric}}});
-//   }
-
-//   const handlePrompt = (promptText) => {
-//     const msg: Message = { id: crypto.randomUUID(),  content: promptText, role: 'user' };
-//     append(msg, { options: { body: { useRag, llm, similarityMetric}}});
-//   };
-
-//   return (
-//     <>
-//     <main className="flex h-screen flex-col items-center justify-center">
-//       <section className='chatbot-section flex flex-col origin:w-[800px] w-full origin:h-[735px] h-full rounded-md p-2 md:p-6'>
-//         <div className='chatbot-header pb-6'>
-//           <div className='flex justify-between'>
-//             <div className='flex items-center gap-2'>
-//               <svg width="24" height="25" viewBox="0 0 24 25">
-//                 <path d="M20 9.96057V7.96057C20 6.86057 19.1 5.96057 18 5.96057H15C15 4.30057 13.66 2.96057 12 2.96057C10.34 2.96057 9 4.30057 9 5.96057H6C4.9 5.96057 4 6.86057 4 7.96057V9.96057C2.34 9.96057 1 11.3006 1 12.9606C1 14.6206 2.34 15.9606 4 15.9606V19.9606C4 21.0606 4.9 21.9606 6 21.9606H18C19.1 21.9606 20 21.0606 20 19.9606V15.9606C21.66 15.9606 23 14.6206 23 12.9606C23 11.3006 21.66 9.96057 20 9.96057ZM7.5 12.4606C7.5 11.6306 8.17 10.9606 9 10.9606C9.83 10.9606 10.5 11.6306 10.5 12.4606C10.5 13.2906 9.83 13.9606 9 13.9606C8.17 13.9606 7.5 13.2906 7.5 12.4606ZM16 17.9606H8V15.9606H16V17.9606ZM15 13.9606C14.17 13.9606 13.5 13.2906 13.5 12.4606C13.5 11.6306 14.17 10.9606 15 10.9606C15.83 10.9606 16.5 11.6306 16.5 12.4606C16.5 13.2906 15.83 13.9606 15 13.9606Z" />
-//               </svg>
-//               <h1 className='chatbot-text-primary text-xl md:text-2xl font-medium'>Chatbot</h1>
-//             </div>
-//             <div className='flex gap-1'>
-//               <ThemeButton />
-//             </div>
-//           </div>
-//           <p className="chatbot-text-secondary-inverse text-sm md:text-base mt-2 md:mt-4">Chatting with the Astra chatbot is a breeze! Simply type your questions or requests in a clear and concise manner. Responses are sourced from Astra documentation and a link for further reading is provided.</p>
-//         </div>
-//         <div className='flex-1 relative overflow-y-auto my-4 md:my-6'>
-//           <div className='absolute w-full overflow-x-hidden'>
-//             {messages.map((message, index) => <Bubble ref={messagesEndRef} key={`message-${index}`} content={message} />)}
-//           </div>
-//         </div>
-//         {!messages || messages.length === 0 && (
-//           <PromptSuggestionRow onPromptClick={handlePrompt} />
-//         )}
-//         <form className='flex h-[40px] gap-2' onSubmit={handleSend}>
-//           <input onChange={handleInputChange} value={input} className='chatbot-input flex-1 text-sm md:text-base outline-none bg-transparent rounded-md p-2' placeholder='Send a message...' />
-//           <button type="submit" className='chatbot-send-button flex rounded-md items-center justify-center px-2.5 origin:px-3'>
-//             <svg width="20" height="20" viewBox="0 0 20 20">
-//               <path d="M2.925 5.025L9.18333 7.70833L2.91667 6.875L2.925 5.025ZM9.175 12.2917L2.91667 14.975V13.125L9.175 12.2917ZM1.25833 2.5L1.25 8.33333L13.75 10L1.25 11.6667L1.25833 17.5L18.75 10L1.25833 2.5Z" />
-//             </svg>
-//             <span className='hidden origin:block font-semibold text-sm ml-2'>Send</span>
-//           </button>
-//         </form>
-//         <Footer />
-//       </section>
-//     </main>
-//     </>
-//   )
-// }
-
 export default function Home() {
-  const [query, setQuery] = useState('')
-  const [result, setResult] = useState('')
-  async function createIndexAndEmbeddings() {
-    try {
-      const result = await fetch('/api/setup', {
-        method: "POST"
-      })
-      const json = await result.json()
-      console.log('result: ', json)
-    } catch (err) {
-      console.log('err:', err)
-    }
+  const { append, messages, input, handleInputChange, handleSubmit } = useChat();
+  const { useRag, llm, similarityMetric, setConfiguration } = useConfiguration();
+
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const handleSend = (e) => {
+    handleSubmit(e, { options: { body: { useRag, llm, similarityMetric } } });
   }
-  async function sendQuery() {
-    if (!query) return
-    setResult('')
-    try {
-      const result = await fetch('/api/chat', {
-        method: "POST",
-        body: JSON.stringify(query)
-      })
-      
-      const json = await result.json()
-      setResult(json.data)
-    } catch (err) {
-      console.log('err:', err)
-    }
-  }
+
+  const handlePrompt = (promptText) => {
+    const msg: Message = { id: crypto.randomUUID(), content: promptText, role: 'user' };
+    append(msg, { options: { body: { useRag, llm, similarityMetric } } });
+  };
   return (
-    <main className="flex flex-1 flex-col justify-center items-center p-24">
-      <p
-        className='text-xl font-medium'
-      >
-      Langchain, Pinecone, and GPT with Next.js
-      </p>
-      <input
-      className='
-      mt-3
-      rounded border w-[400px]
-      text-black px-2 py-1' onChange={e => setQuery(e.target.value)} />
-      <button
-      className='w-[400px] mt-3'
-      onClick={sendQuery}>Ask AI</button>
-      {
-        result && (
-          <p className='my-8 border p-4 rounded'>{result}</p>
-        )
-      }
-      { /* consider removing this button from the UI once the embeddings are created ... */}
-      <button
-      className='w-[400px] mt-2'
-      onClick={createIndexAndEmbeddings}>Create index and embeddings</button>
-    </main>
+    <>
+      <main className="flex h-screen flex-col items-center justify-center">
+        <section className='chatbot-section flex flex-col origin:w-[800px] w-full origin:h-[735px] h-full rounded-md p-2 md:p-6'>
+          <div className='chatbot-header pb-6'>
+            <div className='flex justify-between'>
+              <div className='flex items-center gap-2'>
+                <svg width="24" height="25" viewBox="0 0 24 25">
+                  <path d="M20 9.96057V7.96057C20 6.86057 19.1 5.96057 18 5.96057H15C15 4.30057 13.66 2.96057 12 2.96057C10.34 2.96057 9 4.30057 9 5.96057H6C4.9 5.96057 4 6.86057 4 7.96057V9.96057C2.34 9.96057 1 11.3006 1 12.9606C1 14.6206 2.34 15.9606 4 15.9606V19.9606C4 21.0606 4.9 21.9606 6 21.9606H18C19.1 21.9606 20 21.0606 20 19.9606V15.9606C21.66 15.9606 23 14.6206 23 12.9606C23 11.3006 21.66 9.96057 20 9.96057ZM7.5 12.4606C7.5 11.6306 8.17 10.9606 9 10.9606C9.83 10.9606 10.5 11.6306 10.5 12.4606C10.5 13.2906 9.83 13.9606 9 13.9606C8.17 13.9606 7.5 13.2906 7.5 12.4606ZM16 17.9606H8V15.9606H16V17.9606ZM15 13.9606C14.17 13.9606 13.5 13.2906 13.5 12.4606C13.5 11.6306 14.17 10.9606 15 10.9606C15.83 10.9606 16.5 11.6306 16.5 12.4606C16.5 13.2906 15.83 13.9606 15 13.9606Z" />
+                </svg>
+                <h1 className='chatbot-text-primary text-xl md:text-2xl font-medium'>Chatbot</h1>
+              </div>
+              <div className='flex gap-1'>
+                <ThemeButton />
+              </div>
+            </div>
+            <p className="chatbot-text-secondary-inverse text-sm md:text-base mt-2 md:mt-4">Chatting with the Astra chatbot is a breeze! Simply type your questions or requests in a clear and concise manner. Responses are sourced from Astra documentation and a link for further reading is provided.</p>
+          </div>
+          <div className='flex-1 relative overflow-y-auto my-4 md:my-6'>
+            <div className='absolute w-full overflow-x-hidden'>
+              {messages.map((message, index) => <Bubble ref={messagesEndRef} key={`message-${index}`} content={message} />)}
+            </div>
+          </div>
+          {!messages || messages.length === 0 && (
+            <PromptSuggestionRow onPromptClick={handlePrompt} />
+          )}
+          <form className='flex h-[40px] gap-2' onSubmit={handleSend}>
+            <input onChange={handleInputChange} value={input} className='chatbot-input flex-1 text-sm md:text-base outline-none bg-transparent rounded-md p-2' placeholder='Send a message...' />
+            <button type="submit" className='chatbot-send-button flex rounded-md items-center justify-center px-2.5 origin:px-3'>
+              <svg width="20" height="20" viewBox="0 0 20 20">
+                <path d="M2.925 5.025L9.18333 7.70833L2.91667 6.875L2.925 5.025ZM9.175 12.2917L2.91667 14.975V13.125L9.175 12.2917ZM1.25833 2.5L1.25 8.33333L13.75 10L1.25 11.6667L1.25833 17.5L18.75 10L1.25833 2.5Z" />
+              </svg>
+              <span className='hidden origin:block font-semibold text-sm ml-2'>Send</span>
+            </button>
+          </form>
+          <Footer />
+        </section>
+      </main>
+    </>
   )
 }
